afb5c48351607986e9f95cbb317dd5fb1838b6db
Shubham patel <shubhampatel0305@gmail.com>
2023-07-18 01:33:44 +0530

    Update user.js (remove same site cookies)

--- controller/user.js
+++ controller/user.js
@@ -61,8 +61,7 @@ export const logout = (req, res) => {
   .status(200)
   .cookie("token", "", {
     expires: new Date(Date.now()),
-    sameSite: process.env.NODE_ENV === "Develpoment" ? "lax" : "none",
-    secure: process.env.NODE_ENV === "Develpoment" ? false : true,
+  
   })
   .json({
     success: true,
